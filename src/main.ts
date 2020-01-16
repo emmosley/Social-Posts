b1a339c355f254095c8a7f3380a88f44c1cf9879
Elizabeth <elizabeth.mosley136@gmail.com>
2020-01-15 19:10:38 -0500

    initial commit to start project

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,8 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  interface Post {
+    title: string;
+    thought:string;
+  }
